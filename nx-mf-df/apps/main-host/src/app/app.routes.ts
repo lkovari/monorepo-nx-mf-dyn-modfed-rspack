741f82be81ef4e1e9c80ff7986d3d9fed22f6047
László Kővári <laszlo.kovary@gmail.com>
2026-04-05 13:17:35 +0200

    build scripts, and error fix

--- nx-mf-df/apps/main-host/src/app/app.routes.ts
+++ nx-mf-df/apps/main-host/src/app/app.routes.ts
@@ -1,20 +1,32 @@
+import { Type } from '@angular/core';
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nx/angular/mf';
+
+interface RemoteAppComponentModule {
+    App: Type<unknown>;
+}
 
 export const appRoutes: Route[] = [
     {
         path: 'mf_remote_a',
         loadComponent: () =>
-            loadRemoteModule('mf_remote_a', './Component').then((m) => m.App),
+            loadRemoteModule('mf_remote_a', './Component').then(
+                (m: RemoteAppComponentModule) => m.App,
+            ),
     },
     {
         path: 'mf_remote_b',
         loadComponent: () =>
-            loadRemoteModule('mf_remote_b', './Component').then((m) => m.App),
+            loadRemoteModule('mf_remote_b', './Component').then(
+                (m: RemoteAppComponentModule) => m.App,
+            ),
     },
     {
         path: 'mf_remote_c',
         loadComponent: () =>
-            loadRemoteModule('mf_remote_c', './Component').then((m) => m.App),
+            loadRemoteModule('mf_remote_c', './Component').then(
+                (m: RemoteAppComponentModule) => m.App,
+            ),
     },
     {
         path: '',
